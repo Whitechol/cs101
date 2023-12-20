eb4140c267d3cfd9e9513dbf4be9a09c2af5de92
Whitechol <151142782+Whitechol@users.noreply.github.com>
2023-12-20 18:48:28 +0800

    hw5

--- hw5/main4.c
+++ hw5/main4.c
@@ -4,6 +4,7 @@ int main() {
     int i = 12345;
     int tmp1 = 0, tmp2 = 0;
     int count1 = 0, count2 = 0;
+    
     while (i >= 10000)
     {
         i-=10000;
@@ -24,6 +25,8 @@ int main() {
         i--;
         count2++;
     }
+    
     printf("%d", count1 + tmp2 + count2*1000 + tmp1);
+    
     return 0;
 }
